62fe032f70eaf54c9fbc6d39fa9bc8ccc8699776
KTo <kto@lovetou.ru>
2022-08-02 17:55:44 +0700

    Сериалайзер и представление

--- todo/usersapp/views.py
+++ todo/usersapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from todo.usersapp.models import User
+from todo.usersapp.serializers import UserModelSerializer
+
+
+class UserModelViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
+
