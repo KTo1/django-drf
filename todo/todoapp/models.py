93950f37caec25752d41dad20a2a5e0765175742
KTo <kto@lovetou.ru>
2022-08-10 21:25:38 +0700

    Новое приложение todoappб модели проекта и заметки

---
+++ todo/todoapp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from usersapp.models import User
+
+
+class Project(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=128)
+    repo = models.URLField(verbose_name='Репозиторий', blank=True)
+    users = models.ManyToManyField(User, verbose_name='Пользователи')
+
+
+class ToDo(models.Model):
+    project = models.ForeignKey(Project, verbose_name='Проект', on_delete=models.CASCADE)
+    subject = models.TextField(verbose_name='Текст', blank=True, null=False)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    created = models.DateTimeField(verbose_name='Cоздана', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='Обновлена', auto_now=True)
+    is_active = models.BooleanField(verbose_name='Активна', default=True)
+
+
+
+
