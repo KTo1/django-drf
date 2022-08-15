d2dac5bd5bbeec023914bde6fb0195cc55a75884
KTo <kto@lovetou.ru>
2022-08-16 00:15:52 +0700

    Фильтр и пагинация для проектов
    модель Project: доступны все варианты запросов; для постраничного вывода
    установить размер страницы 10 записей; добавить фильтрацию по совпадению части
    названия проекта;
    
    Пример запроса: http://127.0.0.1:8000/api/projects/?name=Proj

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.renderers import JSONRenderer, AdminRenderer
 from rest_framework.viewsets import ModelViewSet
 
@@ -6,10 +7,23 @@ from .models import Project, ToDo
 from .serializers import ProjectHyperlinkedModelSerializer, ToDoModelSerializer
 
 
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
 class ProjectModelViewSet(ModelViewSet):
-    renderer_classes = [AdminRenderer, JSONRenderer]
+    # renderer_classes = [AdminRenderer, JSONRenderer]
     queryset = Project.objects.all()
     serializer_class = ProjectHyperlinkedModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+
+    def get_queryset(self):
+        name = self.request.query_params.get('name', '')
+        elements = Project.objects.all()
+        if name:
+            elements = elements.filter(name__contains=name)
+
+        return elements
 
 
 class ToDoModelViewSet(ModelViewSet):
