0529083ad1277744bf256d7419d0309e5fba731b
KTo <kto@lovetou.ru>
2022-08-16 00:44:56 +0700

    Фильтрация и пагинация модели ToDo.
    модель ToDo: доступны все варианты запросов; при удалении не удалять ToDo, а
    выставлять признак, что оно закрыто; добавить фильтрацию по проекту; для
    постраничного вывода установить размер страницы 20.
    
    пример запроса: http://127.0.0.1:8000/api/todos/?project=2
    
    Задал basename для роутов, тогда во вьюхаха можно не указывать queryset.

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.renderers import JSONRenderer, AdminRenderer
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Project, ToDo
@@ -13,7 +16,7 @@ class ProjectLimitOffsetPagination(LimitOffsetPagination):
 
 class ProjectModelViewSet(ModelViewSet):
     # renderer_classes = [AdminRenderer, JSONRenderer]
-    queryset = Project.objects.all()
+    # queryset = Project.objects.all()
     serializer_class = ProjectHyperlinkedModelSerializer
     pagination_class = ProjectLimitOffsetPagination
 
@@ -26,6 +29,33 @@ class ProjectModelViewSet(ModelViewSet):
         return elements
 
 
+class ToDoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
+
 class ToDoModelViewSet(ModelViewSet):
-    queryset = ToDo.objects.all()
+    # queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
+    pagination_class = ToDoLimitOffsetPagination
+
+    def destroy(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if pk:
+            element = get_object_or_404(ToDo, pk=pk)
+            element.is_active = False
+            element.save()
+
+            elements = ToDo.objects.all()
+            serializer_class = ToDoModelSerializer(elements, many=True)
+            
+            return Response(serializer_class.data)
+        else:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def get_queryset(self):
+        project = self.request.query_params.get('project', '')
+        elements = ToDo.objects.all()
+        if project:
+            elements = elements.filter(project=project)
+
+        return elements
