8311d6dd6115a86345920d1152be7ffc68636e85
KTo <kto@lovetou.ru>
2022-08-30 14:09:54 +0700

    Тесты APIRequestFactory

--- todo/todoapp/tests.py
+++ todo/todoapp/tests.py
@@ -1,3 +1,75 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import force_authenticate, APIRequestFactory
+from .models import ToDo, Project
+from .views import ToDoModelViewSet, ProjectModelViewSet
+from usersapp.models import User
+
 
 # Create your tests here.
+class TestToDoModelViewSet(TestCase):
+    def setUp(self) -> None:
+        self.url = '/api/todos/'
+        self.factory = APIRequestFactory()
+        self.format = 'json'
+        self.admin = User.objects.create_superuser('kto','kto@kto.ru', 'kto@kto.rukto@kto.ru')
+        self.project = Project.objects.create(name='project1', repo='')
+        self.data = {'project':self.project.id, 'subject':'subject1', 'user': self.admin.id, 'is_active':True}
+
+    #ApiRequestFactory
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get(self.url)
+        view = ToDoModelViewSet.as_view({'get':'list'})
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_admin(self):
+        request = self.factory.post(self.url, self.data, format=self.format)
+        force_authenticate(request, self.admin)
+
+        view = ToDoModelViewSet.as_view({'post':'create'})
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def tearDown(self) -> None:
+        pass
+
+
+class TestProjectModelViewSet(TestCase):
+    def setUp(self) -> None:
+        self.url = '/api/projects/'
+        self.factory = APIRequestFactory()
+        self.format = 'json'
+        self.admin = User.objects.create_superuser('kto', 'kto@kto.ru', 'kto@kto.rukto@kto.ru')
+        self.data = {'name': 'project1', 'repo': '', 'users': [self.admin.id]}
+
+    #ApiRequestFactory
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get(self.url)
+        view = ProjectModelViewSet.as_view({'get':'list'})
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_guest(self):
+        request = self.factory.post(self.url, self.data, format=self.format)
+        view = ProjectModelViewSet.as_view({'post':'create'})
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_admin(self):
+        request = self.factory.post(self.url, self.data, format=self.format)
+        force_authenticate(request, self.admin)
+
+        view = ProjectModelViewSet.as_view({'post':'create'})
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def tearDown(self) -> None:
+        pass
--- todo/usersapp/tests.py
+++ todo/usersapp/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory
+from .models import User
+from .views import UserModelViewSet
+
 
 # Create your tests here.
+class TestUserModelViewSet(TestCase):
+    def setUp(self) -> None:
+        self.factory = APIRequestFactory()
+        self.url = '/api/users/'
+        self.format = 'json'
+        self.data = {'username':'kto1', 'email':'kto1@kto.ru', 'password':'kto1@kto.ru'}
+
+    #ApiRequestFactory
+    def test_get_list(self):
+        request =self.factory.get(self.url)
+        view = UserModelViewSet.as_view({'get':'list'})
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    #Создавать пользователей запрещено
+    def test_create_guest(self):
+        pass
+        # request = self.factory.post(self.url, self.data, format=self.format)
+        # view = UserModelViewSet.as_view({'post':'create'})
+        # response = view(request)
+        #
+        # self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def tearDown(self) -> None:
+        pass
+
