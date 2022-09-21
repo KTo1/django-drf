96d7ce2e79087d840423c735cab5271d0d766842
KTo <kto@lovetou.ru>
2022-09-21 23:02:31 +0700

    Для докера, вынес настройки

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -34,7 +34,11 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.0:3000',
+    'http://0.0.0.0:80',
+    'http://192.168.25.109:8000',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
