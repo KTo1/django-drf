8014df53fe1f7fd3a38cadd8a3df2a7bcc24bcca
KTo <kto@lovetou.ru>
2022-09-21 19:56:05 +0700

    запуск фронта на беке )

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'frontend/build/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -147,6 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (BASE_DIR/'frontend/build/static/', )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
